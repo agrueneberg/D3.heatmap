d23571ed85e14ba1996ba7a2bc38e54b2c458457
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2012-04-27 16:42:29 -0500

    Improve tooltips.

--- heatmap.js
+++ heatmap.js
@@ -11,13 +11,35 @@
  // @param {number} options.height Height of the SVG element.
     exports.heatmap = function (data, options) {
 
-        var parentElement, width, height, colorScale, numRects, rectWidth, rectHeight, svg;
+        var parentElement, width, height, tooltip, colorScale, numRects, rectWidth, rectHeight, svg;
 
      // Evaluate options and set defaults.
         options = options || {};
         parentElement = options.parentElement || document.body;
         width = options.width || 300;
         height = options.height || 300;
+        tooltip = options.tooltip || (function () {
+            var body, tooltip;
+            body = d3.select("body");
+            tooltip = body.append("div")
+                          .style("display", "none")
+                          .style("position", "absolute")
+                          .style("padding", "5px")
+                          .style("background-color", "rgba(242, 242, 242, .6)");
+            return {
+                show: function (d) {
+                    var mouse;
+                    mouse = d3.mouse(body.node());
+                    tooltip.style("display", null)
+                           .style("left", (mouse[0] + 25) + "px")
+                           .style("top", (mouse[1] - 10) + "px")
+                           .html(d);
+                },
+                hide: function () {
+                    tooltip.style("display", "none");
+                }
+            };
+        }());
 
      // Create padded scale for the x axis.
         colorScale = d3.scale.linear()
@@ -58,9 +80,11 @@
                .attr("width", rectWidth)
                .attr("height", rectHeight)
                .attr("fill", colorScale)
-               .append("svg:title")
-               .text(function (d, i) {
-                    return rowLabel + "  and  " + columnLabels[i];
+               .on("mousemove", function (d, i) {
+                   tooltip.show(rowLabel + " and " + columnLabels[i] + ":<br>" + d);
+                })
+               .on("mouseout", function (d) {
+                   tooltip.hide();
                 });
         });
 
