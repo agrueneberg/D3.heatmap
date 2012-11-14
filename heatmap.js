5c8dc59bcfcda24925628f6dce733b660e55c1b7
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2012-11-14 11:27:22 -0600

    Use Canvas instead of SVG to draw the heatmap.

--- heatmap.js
+++ heatmap.js
@@ -6,7 +6,7 @@
 
     exports.heatmap = function () {
 
-        var heatmap, width, height, xScale, yScale, colorScale, tooltip;
+        var heatmap, width, height, xScale, yScale, colorScale;
 
         width = 300;
         height = 300;
@@ -15,45 +15,14 @@
         yScale = d3.scale.ordinal();
         colorScale = d3.scale.linear();
 
-        tooltip = (function () {
-            var body, tooltip;
-            body = d3.select("body");
-            tooltip = body.append("div")
-                          .style("display", "none")
-                          .style("position", "absolute")
-                          .style("padding", "5px")
-                          .style("background-color", "rgba(242, 242, 242, .6)")
-            return {
-                show: function (d) {
-                    var mouse;
-                    mouse = d3.mouse(body.node());
-                    tooltip.style("display", null)
-                           .style("left", (mouse[0] + 25) + "px")
-                           .style("top", (mouse[1] - 10) + "px")
-                           .html(d);
-                },
-                hide: function () {
-                    tooltip.style("display", "none");
-                }
-            };
-        }());
-
         heatmap = function (selection) {
 
             selection.each(function (data) {
 
-                var labels, values, svg, squares;
+                var labels, values, canvas;
 
              // Extract labels.
                 labels = d3.keys(data);
-
-             // Transform data.
-                values = [];
-                labels.map(function (rowLabel) {
-                    labels.map(function (columnLabel) {
-                        values.push([rowLabel, columnLabel, data[rowLabel][columnLabel]]);
-                    });
-                });
 
              // Update x scale.
                 xScale.domain(labels)
@@ -68,53 +37,34 @@
                           .range(["green", "black", "red"]);
 
              // Generate canvas.
-                svg = d3.select(this)
-                        .selectAll("svg")
-                        .data([data]);
+                canvas = d3.select(this)
+                           .selectAll("canvas")
+                           .data([data]);
 
              // Generate chart template.
-                svg.enter()
-                   .append("svg")
-                   .append("g");
+                canvas.enter()
+                      .append("canvas");
 
              // Update dimensions.
-                svg.attr("width", width)
-                   .attr("height", height);
+                canvas.attr("width", width)
+                      .attr("height", height);
 
-             // Generate squares.
-                squares = svg.select("g")
-                             .selectAll("rect")
-                             .data(values, function (d) {
-                                 return [d[0], d[1]];
-                              });
-
-                squares.enter()
-                       .append("rect");
-
-                squares.attr("x", function (d) {
-                           return xScale(d[0]);
-                        })
-                       .attr("y", function (d) {
-                           return yScale(d[1]);
-                        })
-                       .attr("width", function (d) {
-                           return xScale.rangeBand();
-                        })
-                       .attr("height", function (d) {
-                           return yScale.rangeBand();
-                        })
-                       .attr("fill", function (d) {
-                           return colorScale(d[2]);
-                        })
-                       .on("mousemove", function (d) {
-                           tooltip.show(d[0] + " and " + d[1] + ":<br>" + d[2]);
-                        })
-                       .on("mouseout", function (d) {
-                           tooltip.hide();
+                canvas.each(function (d) {
+                    var ctx;
+                    ctx = this.getContext("2d");
+                    labels.forEach(function (l1, xi) {
+                        labels.forEach(function (l2, yi) {
+                            ctx.fillStyle = colorScale(data[l1][l2]);
+                            ctx.fillRect (xScale(xi), yScale(yi), xScale.rangeBand(), yScale.rangeBand());
                         });
-
-                squares.exit()
-                       .remove();
+                    });
+                    this.addEventListener("mousemove", function (e) {
+                        var x, y;
+                        x = Math.floor((e.pageX - this.offsetLeft) / xScale.rangeBand());
+                        y = Math.floor((e.pageY - this.offsetTop) / yScale.rangeBand());
+                        this.title = labels[x] + " and " + labels[y];
+                    });
+                });
 
             });
 
@@ -129,12 +79,6 @@
         heatmap.width = function (_) {
             if (!arguments.length) return width;
             width = _;
-            return heatmap;
-        };
-
-        heatmap.tooltip = function (_) {
-            if (!arguments.length) return tooltip;
-            tooltip = _;
             return heatmap;
         };
 
